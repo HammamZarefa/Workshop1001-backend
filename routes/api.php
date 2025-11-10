3f9065f0b342c7bd4d4021cb493723c59813939f
nada <jboornada79@gmail.com>
2025-11-10 14:39:57 +0300

    feature/Add Route logout

--- routes/api.php
+++ routes/api.php
@@ -5,15 +5,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
     Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
-
-    Route::post('/logout', [AuthController::class, 'logout']);
-      Route::get('/test-api', [AuthController::class, 'test']);
-      
-Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    
+    Route::get('/test-api', [AuthController::class, 'test']);
+    Route::middleware('auth:sanctum')->group(function () {
     Route::get('/profile', [AuthController::class, 'profile']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
-
-
 });
 
