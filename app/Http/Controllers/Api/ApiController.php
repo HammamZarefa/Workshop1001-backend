49ea37ba11abf68ea3063ae9f2857a073b109d2e
unknown <tamemabozen@gmail.com>
2025-11-15 14:57:43 +0300

    build apicontroller used trait without update the controllers

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Traits\ApiResponses;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+   use ApiResponses;
+}
