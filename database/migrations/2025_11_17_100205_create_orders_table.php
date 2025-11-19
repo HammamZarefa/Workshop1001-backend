4704060a55df2a9401a44baad6117e035a5f796a
unknown <tamemabozen@gmail.com>
2025-11-19 12:55:02 +0300

    create coupons module

--- database/migrations/2025_11_17_100205_create_orders_table.php
+++ database/migrations/2025_11_17_100205_create_orders_table.php
@@ -10,7 +10,7 @@ return new class extends Migration {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
-            $table->foreignId('coupon_id')->constrained('coupons')->cascadeOnDelete();
+//            $table->foreignId('coupon_id')->constrained('coupons')->cascadeOnDelete()->nullable();
             $table->decimal('coupon_value', 10, 2)->default(0);
             $table->string('shipping_address');
             $table->enum('status', ['pending', 'accepted', 'rejected'])->default('pending');
